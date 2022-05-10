289c486ed83005014d50b12b8bdf48431bdfd6f3
Luke Jennings <lukejennings123@gmail.com>
2022-05-10 16:39:19 +0100

    added logic to enable times, multiply, divide, subtract to work on numbers when the equals button is pressed

--- logic.js
+++ logic.js
@@ -14,58 +14,73 @@ const minus = document.querySelector('.minus');
 const division = document.querySelector('.divide');
 const times = document.querySelector('.multiply');
 const ac = document.querySelector('.AC');
+const equal = document.querySelector('.equal');
+const decimal = document.querySelector('.decimal');
+
+const numInput1 = number1.addEventListener('click', () =>  screenSelect.textContent += '1');
+const numInput2 = number2.addEventListener('click', () =>  screenSelect.textContent += '2');
+const numInput3 = number3.addEventListener('click', () =>  screenSelect.textContent += '3');
+const numInput4 = number4.addEventListener('click', () =>  screenSelect.textContent += '4');
+const numInput5 = number5.addEventListener('click', () =>  screenSelect.textContent += '5');
+const numInput6 = number6.addEventListener('click', () =>  screenSelect.textContent += '6');
+const numInput7 = number7.addEventListener('click', () =>  screenSelect.textContent += '7');
+const numInput8 = number8.addEventListener('click', () =>  screenSelect.textContent += '8');
+const numInput9 = number9.addEventListener('click', () =>  screenSelect.textContent += '9');
+const numInput0 = number0.addEventListener('click', () =>  screenSelect.textContent += '0');
+const numInput10 = plus.addEventListener('click', () =>  screenSelect.textContent += '+');
+const numInput11 = minus.addEventListener('click', () =>  screenSelect.textContent += '-');
+const numInput12 = division.addEventListener('click', () =>  screenSelect.textContent += '/');
+const numInput13 = times.addEventListener('click', () =>  screenSelect.textContent += '*' );
+const numInput14 = decimal.addEventListener('click', () =>  screenSelect.textContent += '.' );
+const screenContent = screenSelect;
+
+let resetScreen =  ac.addEventListener('click', () =>  screenSelect.innerHTML = '');
 
 
+let firstInput = 0;
+let secondInput = 0;
+let operator = '';
 
-let screenContent = screenSelect;
-
-let numInput1 = number1.addEventListener('click', () =>  screenSelect.textContent += '1');
-let numInput2 = number2.addEventListener('click', () =>  screenSelect.textContent += '2');
-let numInput3 = number3.addEventListener('click', () =>  screenSelect.textContent += '3');
-let numInput4 = number4.addEventListener('click', () =>  screenSelect.textContent += '4');
-let numInput5 = number5.addEventListener('click', () =>  screenSelect.textContent += '5');
-let numInput6 = number6.addEventListener('click', () =>  screenSelect.textContent += '6');
-let numInput7 = number7.addEventListener('click', () =>  screenSelect.textContent += '7');
-let numInput8 = number8.addEventListener('click', () =>  screenSelect.textContent += '8');
-let numInput9 = number9.addEventListener('click', () =>  screenSelect.textContent += '9');
-let numInput0 = number0.addEventListener('click', () =>  screenSelect.textContent += '0');
-let numInput10 = plus.addEventListener('click', () =>  screenSelect.textContent += '+');
-let numInput11 = minus.addEventListener('click', () =>  screenSelect.textContent += '-');
-let numInput12 = division.addEventListener('click', () =>  screenSelect.textContent += '/');
-let numInput13 = times.addEventListener('click', () =>  screenSelect.textContent += '*' );
-
-storage = 0
 let plusLogic = plus.addEventListener('click', () => {
-    const Select = document.querySelector('.screen').innerHTML;
-    return storage += parseInt(Select)
+    const select = document.querySelector('.screen').innerHTML;
+    return firstInput += parseFloat(select), screenSelect.innerHTML = '', operator += '+'
+}); //this takes the first input and stores it in the firstInput variable once + has been selected
+let subtractLogic = division.addEventListener('click', () => {
+    const select = document.querySelector('.screen').innerHTML;
+    return firstInput += parseFloat(select), screenSelect.innerHTML = '', operator += '/'
+}); 
+let divideLogic = minus.addEventListener('click', () => {
+    const select = document.querySelector('.screen').innerHTML;
+    return firstInput += parseFloat(select), screenSelect.innerHTML = '', operator += '-'
+}); 
+let multiplyLogic = times.addEventListener('click', () => {
+    const select = document.querySelector('.screen').innerHTML;
+    return firstInput += parseFloat(select), screenSelect.innerHTML = '', operator += '*'
+}); 
+let secondInputReturn = equal.addEventListener('click', () => {
+    const screenSelector = document.querySelector('.screen').innerHTML;
+    return secondInput += parseFloat(screenSelector), screenSelect.innerHTML = ''
+}); // this takes the second input and assigns it to the secondInput variable when equals is pressed.
+
+let displayOutput = equal.addEventListener('click', () => {
+    finalFiquire = 0;
+    if (operator === '+') {
+    return scoreAdd = finalFiquire += (firstInput + secondInput),
+    screenSelect.textContent += scoreAdd, firstInput = 0, secondInput = 0, operator = '';}
+    { if   
+    (operator === '-')
+    return  scoreSubtract = finalFiquire += (firstInput - secondInput),
+    screenSelect.textContent += scoreSubtract, firstInput = 0, secondInput = 0, operator = '';}
+    { if   
+    (operator === '/')
+    return  scoreDivide = finalFiquire += (firstInput / secondInput),
+    screenSelect.textContent += scoreDivide, firstInput = 0, secondInput = 0, operator = '';}
+    { if   
+    (operator === '*')
+    return  scoreDivide = finalFiquire += (firstInput * secondInput),
+    screenSelect.textContent += scoreDivide, firstInput = 0, secondInput = 0, operator = '';}
 });
 
-let resetScreen =  ac.addEventListener('click', () => screenSelect.innerHTML = '');
-
-
-const add = (num1, num2) => { return num1 + num2  };
-const subtract = (num1, num2) => { return num1 - num2 };
-const multiply = (num1, num2) => { return num1 * num2 };
-const divide = (num1, num2) => { return num1 / num2 };
-
-
-
-const computer = function operator(input1,opInput,input2){
- if (opInput === '+') {
-  return add(input1, input2); }
-
-  else if (opInput === '-') {
-  return subtract(input1, input2); }
-
-  else if (opInput === '*') {
-    return multiply(input1, input2); }
-
-  else if  (opInput === '/') {
-    return divide(input1, input2); }
-};
-/* the function needs to take the first input from numinput and add that as a variable 
-to input1 or input2*, this needs a new click event that fires simultaneously to the 
-.text content numInput (as this is just to show the user) */
 
 
 
